9587db139314a55d235396a15d91822bb39292ae
GaboDev <gonzaloramirez.sagitario@gmail.com>
2023-01-20 21:28:27 -0300

    Iniciando proyecto GrowApp

---
+++ js/index.js
@@ -0,0 +1,1 @@
+import {} from "./loader.js";
---
+++ js/loader.js
@@ -0,0 +1,20 @@
+const preloader = document.querySelector('.preloader');
+
+setTimeout(() => {
+    
+    preloader.style = `
+        opacity: 0;
+        visibility: hidden;
+    `;
+
+}, 11000); 
+
+
+// window.onload = ()=>{
+
+//     preloader.style = `
+//         opacity:0;
+//         visibility:hidden;
+//     `;
+
+// }
