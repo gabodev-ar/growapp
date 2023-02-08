2ea0037e8ae12f950fe19c19f0254aed4076b86a
GaboDev <gonzaloramirez.sagitario@gmail.com>
2023-02-08 20:30:32 -0300

    search.js

---
+++ js/search.js
@@ -0,0 +1,9 @@
+const searchInput   = document.getElementById('search');
+const searchBox     = document.querySelector('.search__results');
+const searchResults = document.querySelectorAll('.search__result');
+
+searchInput.addEventListener('keyup', (e)=>{
+    e.preventDefault();
+
+
+})
